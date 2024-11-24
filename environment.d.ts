d88bbe4367e8b600822ca77f166f913908577a24
g_rocket <georgiiromanishko@gmail.com>
2024-11-24 16:13:18 +0100

    feat(project): changing env declare

--- environment.d.ts
+++ environment.d.ts
@@ -1,13 +1,10 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      CORE_APP_PORT: string;
       USER_APP_PORT: string;
       POST_APP_PORT: string;
-      CORE_SERVICE_HOST: string;
       USER_SERVICE_HOST: string;
       POST_SERVICE_HOST: string;
-      CORE_SERVICE_PORT: string;
       USER_SERVICE_PORT: string;
       POST_SERVICE_PORT: string;
       NODE_ENV?: 'production';
