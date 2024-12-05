3ad05346957281b0f03c0041b93f9d2f13841ca4
g_rocket <georgiiromanishko@gmail.com>
2024-12-05 14:40:17 +0100

    feat(project): changing env

--- environment.d.ts
+++ environment.d.ts
@@ -4,6 +4,7 @@ declare global {
       USER_APP_PORT: string;
       POST_APP_PORT: string;
       AUTH_APP_PORT: string;
+      FILE_APP_PORT: string;
       AUTH_SERVICE_HOST: string;
       AUTH_SERVICE_PORT: string;
       JWT_SECRET: string;
